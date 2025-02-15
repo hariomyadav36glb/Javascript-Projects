414e6bf37a5d5c3c572ef3a44393b53d917d8ac9
Hari Om <hariomyadv36@gmail.com>
2025-02-15 15:51:42 +0530

    done

--- New folder/script.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector("form");
-
-form.addEventListener("submit",(event)=>{
-    event.preventDefault();
-    const data = new FormData(form);
-    console.log(Array.from(data.keys()),Array.from(data.values()));
-})
